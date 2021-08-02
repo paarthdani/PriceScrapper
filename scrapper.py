51a267cfe40bc7eb1fbf965346612db538d12453
paarthdani <parth_dani@yahoo.in>
2021-08-02 17:01:05 +0530

    added class search

--- scrapper.py
+++ scrapper.py
@@ -1,9 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://www.amazon.in"
+URL = "https://prsundar.blogspot.com/"
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
 
-print(soup)
+# print(soup)
+
+results = soup.find(id="Blog1")
+job_elements = results.find_all("h3", class_="post-title entry-title item-title")
+
+print(job_elements)
+
+
+
+
+
