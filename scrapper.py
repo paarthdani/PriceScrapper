34a758548ec8e7c3e5a7355561f5c116677c9194
Parth Dani <parth_dani@yahoo.in>
2021-10-26 12:51:14 +0530

    Update scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://prsundar.blogspot.com/"
+URL = "https://www.amazon.in/Wear-Your-Opinion-Printed-T-Shirt/dp/B09BZFM6SY/ref=sr_1_4_sspa?crid=3SRLZDG0M0VNR&dchild=1&keywords=code&qid=1635232842&s=apparel&sprefix=cod%2Cfashion%2C254&sr=1-4-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyUkJSMEVXVzZUMzBKJmVuY3J5cHRlZElkPUEwODQxOTMwMzMxVUJVT0ZJRVc5SyZlbmNyeXB0ZWRBZElkPUEwMTc4MzAwMTJIVEJGSjhFODdXQiZ3aWRnZXROYW1lPXNwX2F0ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU="
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
